d8b1f281c22060c3066345fce0d57667eb347975
Nico <nicholai.gerschewsky@clarilab.de>
2024-06-07 04:12:41 +0200

    feat: add grpc interceptors
    - adds authentication interceptors for unary and stream grpc requests
    - adds some minor code improvments
    - upgrades dependencies

--- gocloaksession.go
+++ gocloaksession.go
@@ -3,15 +3,25 @@ package gocloaksession
 import (
 	"github.com/Nerzal/gocloak/v13"
 	"github.com/go-resty/resty/v2"
+	"google.golang.org/grpc"
 )
 
 // GoCloakSession holds all callable methods
 type GoCloakSession interface {
-	// GetKeycloakAuthToken returns a JWT object, containing the AccessToken and more
+	// GetKeycloakAuthToken returns a JWT object, containing the AccessToken and more.
 	GetKeycloakAuthToken() (*gocloak.JWT, error)
 
-	// AddAuthTokenToRequest sets the Authentication Header for the response
+	// AddAuthTokenToRequest is a middleware for resty that sets the
+	// Authorization header on the request.
 	AddAuthTokenToRequest(*resty.Client, *resty.Request) error
+
+	// GRPCUnaryAuthenticate is an unary client interceptor for setting the
+	// Authorization Header on gRPC requests.
+	GRPCUnaryAuthenticate() grpc.UnaryClientInterceptor
+
+	// GRPCUnaryAuthenticate is a stream client interceptor for that setting the
+	// Authorization Header on gRPC requests.
+	GRPCStreamAuthenticate() grpc.StreamClientInterceptor
 
 	// GetGoCloakInstance returns the currently used GoCloak instance.
 	GetGoCloakInstance() *gocloak.GoCloak
